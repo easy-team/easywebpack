76a597a22cf1c59d26c42e1866b3ca97a2f35e98
caoli <cl98914@alibaba-inc.com>
2018-03-19 19:01:21 +0800

    feat: mp4|webm|ogg|mp3|wav|flac|aac for url-loader

--- config/loader.js
+++ config/loader.js
@@ -99,6 +99,26 @@ exports.urlimage = {
   ]
 };
 
+exports.urlmedia = {
+  enable: true,
+  test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+  use: [
+    {
+      loader: 'url-loader',
+      options: {
+        limit: 1024
+      },
+      fn(){
+        return {
+          options: {
+            name: this.webpackInfo.mediaName
+          }
+        }
+      }
+    }
+  ]
+}
+
 exports.urlfont = {
   enable: true,
   test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
