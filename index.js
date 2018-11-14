fa11a0767c0b95c0195c5eb76e8007fdf5657abe
sky <hubcarl@126.com>
2018-11-14 17:32:24 +0800

    fix: webpack-tool proxy

--- index.js
+++ index.js
@@ -44,10 +44,10 @@ exports.getDllWebpackConfig = (config = {}, option = {}) => {
   return exports.WebpackBuilder.getDllWebpackConfig(config);
 };
 
-exports.build = (webpackConfig, option, callback) => {
-  return new WebpackTool().build(webpackConfig, option, callback);
+exports.build = (webpackConfig, config, callback) => {
+  return new WebpackTool(config).build(webpackConfig, config, callback);
 };
 
-exports.server = (webpackConfig, option, callback) => {
-  return new WebpackTool().server(webpackConfig, option, callback);
+exports.server = (webpackConfig, config, callback) => {
+  return new WebpackTool(config).server(webpackConfig, config, callback);
 };
