b35da9a538c994875daa4cd6f1e9997057dab70e
asins <asinsimple@gmail.com>
2019-06-04 18:40:44 +0800

    browserslist设置项更新

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -3,6 +3,6 @@
 
 module.exports = {
   plugins: [
-    require('autoprefixer')({ browsers: ['iOS >= 7', 'Android >= 4.0'] })
+    require('autoprefixer')({ overrideBrowserslist: ['iOS >= 7', 'Android >= 4.0'] })
   ]
 };
