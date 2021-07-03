a971d7cbdc19ba65b9875c42d8a30e9730af3efb
sky <hubcarl@126.com>
2021-07-03 19:51:50 +0800

    feat: setting vue3 webpack build alias

--- packages/framework/easywebpack-vue/lib/client.js
+++ packages/framework/easywebpack-vue/lib/client.js
@@ -1,5 +1,6 @@
 'use strict';
 const EasyWebpack = require('easywebpack');
+const { isVue3 } = require('./utils');
 const WebpackBaseBuilder = require('./base');
 const babelrc = require('../config/babel.web');
 // eslint-disable-next-line new-cap
@@ -7,7 +8,11 @@ class WebpackClientBuilder extends WebpackBaseBuilder(EasyWebpack.WebpackClientB
   constructor(config) {
     super(config);
     this.setBabelrc(babelrc);
-    this.setAlias('vue', 'vue/dist/vue.common.js', false);
+    if (isVue3(this.baseDir)) {
+      this.setAlias('vue', 'vue/dist/vue.runtime.esm-bundler.js', false);
+    } else {
+      this.setAlias('vue', 'vue/dist/vue.common.js', false);
+    }
   }
 }
 module.exports = WebpackClientBuilder;
